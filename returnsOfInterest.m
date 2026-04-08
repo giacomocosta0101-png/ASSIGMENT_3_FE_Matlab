ba72f3425a88895aec9c5c2c8a4cffbcdec3ed04
Gabriele <gabriele.alippi@mail.polimi.it>
2026-04-08 15:39:11 +0200

    nuova modifica (mancavano dei file)

---
+++ returnsOfInterest.m
@@ -0,0 +1,64 @@
+function [tSelected, returnsSelected, pricesAligned] = returnsOfInterest(inputFile, refDate, timeWindow, sharesList, formatDate)
+% Selects set of dates and returns in a lag of interest
+% [tSel returnsSel] = returnsOfInterest(inputFile, refDate, timeWindow, sharesList)
+%
+% INPUTS:
+% - inputFile:  complete excel file name (with path) 
+% - refDate:    first date of interest
+% - timeWindow: in months
+% - sharesList: list of the N Shares of Interest
+% - formatDate: format used for dates (default 'mm/dd/yyyy')
+% 
+% OUTPUTS:
+% - tSelected:  vector with the T dates of interest 
+% - returnsSel: matrix with TxN returns values
+% - pricesAligned: T×N matrix of aligned prices 
+%
+% USES:
+% - findSeries: given the set of data selects the asset of interest
+% - dateAddMonth: adds to a month a given number of months
+% - closestDate: selects the nearest (even the previous one) to a given date
+% - underlyingCode: Converts share name in bbg code 
+
+if(nargin <5)
+    formatDate = 'mm/dd/yyyy';
+end
+
+elementsBasket = size(sharesList,1);
+
+% Scarico dati storici 
+[shareData.num,shareData.cell]=xlsread(inputFile,'Data','a5:cx1295');
+
+% Select the set of dates of interest: the ones in Eurostoxx50
+[~, t_index]=findSeries(shareData,'SX5E Index', formatDate);
+
+% refDate, endDate
+refDate=datenum(refDate); 
+[refDate, idxStart] = closestDate(refDate, t_index);
+endDate = dateAddMonth(refDate, timeWindow);
+[~, idxEnd] = closestDate(endDate, t_index);
+
+idx1 = min(idxStart, idxEnd);
+idx2 = max(idxStart, idxEnd);
+tSelected = t_index(idx1:idx2);
+
+%tSelected = t_index(idxStart:idxEnd);
+
+% Prices of the selected shares
+% If the value is not present I take the value from the previous date 
+%valuesSelectedShares=zeros(idxEnd-idxStart+1,elementsBasket);
+valuesSelectedShares = zeros(length(tSelected), elementsBasket);
+
+for i = 1:elementsBasket
+    bbgCode = underlyingCode(sharesList(i,:));
+    [values_share, t_share] = findSeries(shareData, bbgCode, formatDate);
+    
+    [~, idxShare] = arrayfun(@(d) closestDate(d, t_share), tSelected);
+    valuesSelectedShares(:, i) = values_share(idxShare);
+end
+
+returnsSelected=log(valuesSelectedShares(2:end,:)./valuesSelectedShares(1:end-1,:));
+tSelected = tSelected(2:end);
+pricesAligned = valuesSelectedShares(1:end, :);
+
+end %returnsOfInterest
